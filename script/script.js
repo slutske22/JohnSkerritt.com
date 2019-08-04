bdaba50a374e392e628a7661a5ff2fcb6113c7de
slutske22 <seth.lutske@gmail.com>
2019-08-04 15:40:56 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -14,3 +14,10 @@ menuButton.addEventListener("mouseout",function(){
 menuButton.addEventListener("click",function(){
    menuButton.classList.add("isOpen");
 },false)
+
+
+var donateButtons = document.querySelector(".donateNav");
+
+donateButtons.addEventListener("click",function(){
+   alert("The Jonathan Skerritt Political Action Campaign (PAC) is not accepting donations at this time due to unresolved allegations against the PAC by the Securities and Exchange Commission.  Check back later.")
+},false);
